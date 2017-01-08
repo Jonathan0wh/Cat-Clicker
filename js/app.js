cf7fb1599692ded095127457bea12b5bf3384567
Jonathan <Jonathan0wh@gmail.com>
2017-01-09 01:24:10 +1100

    add multiple functions, improve MVC, massive ES6 feature

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 let model = {
+
     cats: [
         {
             id: "cat1",
@@ -31,24 +32,53 @@ let model = {
             clickCount: 0
         }
     ],
+
     getCats: function () {
         return this.cats;
+    },
+
+    getCatIndexByName: function (catName) {
+        return this.cats.findIndex((item) => item.name === catName);
+    },
+
+    getCatByIndex: function (catIndex) {
+        return model.getCats()[catIndex];
+    },
+
+    setCatByIndex: function (catIndex, selectedCatName, newCatImg, newCatCount) {
+        Object.assign(this.cats[catIndex],
+        {
+            name: selectedCatName,
+            imgSrc: newCatImg,
+            clickCount: newCatCount
+        });
     }
 };
 
 
 let controller = {
+
     init: function () {
         navView.init();
         tabView.init();
+        buttonView.init();
+        formView.init();
     },
+
     getCats: function () {
         return model.getCats();
+    },
+
+    saveData: function (selectedCatName, newCatImg, newCatCount) {
+        let selectedCatIndex = model.getCatIndexByName(selectedCatName);
+        model.setCatByIndex(selectedCatIndex, selectedCatName, newCatImg, newCatCount);
+        tabView.render(selectedCatIndex);
     }
 };
 
 
 let navView = {
+
     init: function () {
         let navItemModelStr = $("#nav-item-template").html();
 
@@ -67,8 +97,11 @@ let navView = {
 
 
 let tabView = {
+
     init: function () {
         let tabPaneModelStr = $("#tab-pane-template").html();
+
+        $(".cat-tab").empty();
 
         controller.getCats().forEach(function (cat, index) {
             let tabPane = tabPaneModelStr.replace(/%catID%/g, cat.id).replace(/%catName%/g, cat.name).replace(/%catImg%/g, cat.imgSrc);
@@ -88,6 +121,64 @@ let tabView = {
                 $(selectCount).text(cat.clickCount);
             });
         });
+    },
+
+    render: function (selectedCatIndex) {
+        let cat = model.getCatByIndex(selectedCatIndex);
+        let selectedCatID = "#" + cat.id;
+        $(selectedCatID + ">h2").text(cat.name);
+        $(selectedCatID + ">img").attr("src", cat.imgSrc);
+        $(selectedCatID + " strong").text(cat.clickCount);
+    }
+}
+
+let buttonView = {
+
+    init: function () {
+        $("#admin-button").click(function () { 
+            formView.toggleVisibility();
+        });
+    }
+}
+
+let formView = {
+
+    init: function () {
+        $("#input-form").addClass("invisible");
+        $("#cancel-button").click(function () { 
+            formView.hide();
+        });
+        $("#save-button").click(function () { 
+            let selectedCat = $("#cat-name-change").val();
+            let newCatImg = $("#cat-img-change").val();
+            let newCatCount = $("#cat-count-change").val();
+
+            controller.saveData(selectedCat, newCatImg, newCatCount);
+
+            formView.hide();
+            $("#cat-name-change").val("");
+            $("#cat-img-change").val("");
+            $("#cat-count-change").val("");
+
+            let alert = `<div class="alert alert-success" role="alert">
+                            <strong class="text-md-center">Well done!</strong> You successfully changed a cat.
+                        </div>`;
+            $("main").append(alert);
+            $(".alert").alert();
+            window.setTimeout(function () {$(".alert").alert('close');}, 2000);
+        });
+    },
+
+    toggleVisibility: function () {
+        $("#input-form").toggleClass("invisible");
+    },
+    
+    show: function () {
+        $("#input-form").removeClass("invisible");
+    },
+
+    hide: function () {
+        $("#input-form").addClass("invisible");
     }
 }
 
